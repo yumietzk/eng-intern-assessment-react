07000941a8f7a1502acb852d2fc88ba5465343dc
Yumie Tsuzuki <yumie.t.29@gmail.com>
2024-01-15 13:21:41 -0500

    extract logic to a custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
-import React, { useRef, useState } from "react";
+import React from "react";
+import { useTime } from "./hooks/useTime";
 import StopWatch from "./StopWatch";
 import "./App.css";
 
 // The main component that renders the stopwatch and handles its functionality
 export default function App() {
-  const [time, setTime] = useState(0);
-  const [isCounting, setIsCounting] = useState(false);
-  const [laps, setLaps] = useState([]);
-
-  const intervalRef = useRef(null);
-
-  // Start counting
-  function handleStart() {
-    setIsCounting(true);
-
-    clearInterval(intervalRef.current);
-    // Update every 10ms
-    intervalRef.current = setInterval(() => {
-      setTime((t) => t + 1);
-    }, 10);
-  }
-
-  // Stop counting
-  function handleStop() {
-    setIsCounting(false);
-
-    clearInterval(intervalRef.current);
-  }
-
-  // Record laps
-  function handleRecordLaps() {
-    setLaps((l) => [...l, time]);
-  }
-
-  // Reset to zero
-  function handleReset() {
-    setIsCounting(false);
-
-    setTime(0);
-    setLaps([]);
-    clearInterval(intervalRef.current);
-  }
+  const {
+    time,
+    isCounting,
+    laps,
+    handleStart,
+    handleStop,
+    handleRecordLaps,
+    handleReset,
+  } = useTime();
 
   return (
     <StopWatch
---
+++ src/hooks/useTime.tsx
@@ -0,0 +1,51 @@
+import { useRef, useState } from "react";
+
+export function useTime() {
+  const [time, setTime] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
+  const [laps, setLaps] = useState([]);
+
+  const intervalRef = useRef(null);
+
+  // Start counting
+  function handleStart() {
+    setIsCounting(true);
+
+    clearInterval(intervalRef.current);
+    // Update every 10ms
+    intervalRef.current = setInterval(() => {
+      setTime((t) => t + 1);
+    }, 10);
+  }
+
+  // Stop counting
+  function handleStop() {
+    setIsCounting(false);
+
+    clearInterval(intervalRef.current);
+  }
+
+  // Record laps
+  function handleRecordLaps() {
+    setLaps((l) => [...l, time]);
+  }
+
+  // Reset to zero
+  function handleReset() {
+    setIsCounting(false);
+
+    setTime(0);
+    setLaps([]);
+    clearInterval(intervalRef.current);
+  }
+
+  return {
+    time,
+    isCounting,
+    laps,
+    handleStart,
+    handleStop,
+    handleRecordLaps,
+    handleReset,
+  };
+}
